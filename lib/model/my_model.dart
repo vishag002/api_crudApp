397c09b4cf5a0c1e4311832b4373fa95a19d6b5c
vishag002 <vvishag477@gmail.com>
2024-05-28 13:49:59 +0530

    api

---
+++ lib/model/my_model.dart
@@ -0,0 +1,29 @@
+class MyModel {
+  String? name;
+  String? country;
+  String? alpha_two_code;
+  MyModel({this.name, this.country, this.alpha_two_code});
+}
+
+
+  
+/*{ {
+  "domains": [
+  "marywood.edu"
+  ],
+  "name": "Marywood University",
+  "namestate-province": null,
+  "country": "United States",
+  "alpha_two_code": "US",
+  "web_pages": [
+  "http://www.marywood.edu"
+  ]
+  },
+  
+} */
+/* {'name': 'Vishag', 'des': 'futter developer'},
+{'name': 'dev', 'des': 'futter developer'},
+{'name': 'Ram', 'des': 'futter developer'},
+{'name': 'Sita', 'des': 'futter developer'},
+{'name': 'Ajay', 'des': 'futter developer'},
+{'name': 'Ganesh', 'des': 'futter developer'} */
